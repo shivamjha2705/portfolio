4178e7dc2a682bc7d0ac7432f91ce7aa1f4e8803
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-05-25 22:49:30 +0530

    changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,7 +3,7 @@ import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { github, live } from "../assets";
+import {  live } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
@@ -65,11 +65,11 @@ const ProjectCard = ({
                 className="black-gradient border-2 w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
                 title="Source Code"
               >
-                <img
+                {/* <img
                   src={github}
                   alt="source code"
                   className="w-1/2 h-1/2 object-contain"
-                />
+                /> */}
               </div>
             )}
           </div>
