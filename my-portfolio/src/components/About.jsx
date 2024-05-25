83a8591699767864cbeff902e111267bdcf7e30c
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-05-25 20:18:42 +0530

    new portfolio

---
+++ my-portfolio/src/components/About.jsx
@@ -0,0 +1,60 @@
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
+import { SectionWrapper } from '../hoc'
+
+const ServiceCard =({index, title, icon}) => {
+  return(
+    <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+    <div
+       options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+    >
+    <img
+          src={icon}
+          alt={title}
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+
+    </div>
+    </motion.div>
+    </Tilt>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] text-justify'
+      >
+      I am a skilled Software Developer with a strong foundation in web development and data analytics. Proficient in HTML, CSS, JavaScript, React.js, Bootstrap, Tailwind Css, CMS tools like Sanity, and Firebase, I thrive on learning new technologies and applying them to tackle real-world challenges. Ready to contribute as a proactive software developer and engineer, I am committed to delivering innovative solutions that drive progress. My passion for problem-solving and dedication to staying at the forefront of technological advancements fuel my quest for excellence in the software engineering domain.
+      </motion.p>
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services?.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about")
