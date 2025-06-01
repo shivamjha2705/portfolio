1b3972cef8864b019b86de73d380eed7cb0d6b72
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-06-01 12:32:02 +0530

    updated about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unknown-property */
+
 import { Tilt } from 'react-tilt'
 import { motion } from 'framer-motion'
 import { styles } from '../styles'
@@ -5,33 +7,33 @@ import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 import { SectionWrapper } from '../hoc'
 
-const ServiceCard =({index, title, icon}) => {
-  return(
+const ServiceCard = ({ index, title, icon }) => {
+  return (
     <Tilt className="xs:w-[250px] w-full">
-    <motion.div
-      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
-    >
-    <div
-       options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-    >
-    <img
-          src={icon}
-          alt={title}
-          className='w-16 h-16 object-contain'
-        />
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img
+            src={icon}
+            alt={title}
+            className='w-16 h-16 object-contain'
+          />
 
-        <h3 className='text-white text-[20px] font-bold text-center'>
-          {title}
-        </h3>
+          <h3 className='text-white text-[20px] font-bold text-center'>
+            {title}
+          </h3>
 
-    </div>
-    </motion.div>
+        </div>
+      </motion.div>
     </Tilt>
   )
 }
@@ -46,7 +48,9 @@ const About = () => {
       <motion.p variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] text-justify'
       >
-      I am a skilled Software Developer with a strong foundation in web development and data analytics. Proficient in HTML, CSS, JavaScript, React.js, Bootstrap, Tailwind Css, CMS tools like Sanity, and Firebase, I thrive on learning new technologies and applying them to tackle real-world challenges. Ready to contribute as a proactive software developer and engineer, I am committed to delivering innovative solutions that drive progress. My passion for problem-solving and dedication to staying at the forefront of technological advancements fuel my quest for excellence in the software engineering domain.
+        Frontend-focused Software Developer with 1+ year of hands-on experience building scalable, performant, and visually appealing web applications. Skilled in React.js, Tailwind CSS, Redux Toolkit, Firebase, and modern UI libraries like Shadcn and Framer Motion. I’ve delivered 10+ responsive projects in a fast-paced startup environment, led small teams, and optimized systems for speed, accessibility, and user experience.
+      <br/>
+      I thrive on turning complex ideas into intuitive user interfaces and enjoy collaborating with cross-functional teams to bring digital products to life. Passionate about clean code, performance optimization, and continuously learning new technologies to stay ahead in the frontend ecosystem
       </motion.p>
       <div className='mt-20 flex flex-wrap gap-10'>
         {services?.map((service, index) => (
