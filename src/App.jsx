3434b26f4addd8d8dcb06f10b28cd4c49935c450
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-06-01 19:10:14 +0530

    added layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,46 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 // -----all components----
-import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
-const App = ()=> {
+import { useEffect } from "react";
+import Layout from "./components/Layout";
+import ProjectDetails from "./components/pages/ProjectDetails";
+
+
+const ScrollOnLoad = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname.replace('/', '');
+    if (path) {
+      const el = document.getElementById(path);
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [location]);
+
+  return null;
+};
+
+
+const App = () => {
+  // const previousPath = usePreviousPath();
+  
   return (
     <BrowserRouter>
-    <div className="relative z-0 bg-primary">
-    <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-      <Navbar/>
-      <Hero/>
+    <ScrollOnLoad/>
+  <div className="relative z-0 bg-primary">
+    <div className="bg-cover bg-no-repeat bg-center">
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        {/* <Route path="/project" element={<Works />} /> */}
+         {/* New Project Details page */}
+         <Route path="/project/:id" element={<ProjectDetails />} />
+      </Routes>
     </div>
-    <About/>
-    <Experience/>
-    <Tech/>
-    <Works/>
-    <Feedbacks/>
-    <div className="relative z-0">
-      <Contact/>
-      <StarsCanvas/>
-    </div>
-    </div>
-    </BrowserRouter>
-  )
-}
+  </div>
+</BrowserRouter>
 
-export default App
+  );
+};
+
+export default App;
