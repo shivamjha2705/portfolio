64e1689d0771ee32334105a280136c15eb98201f
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-06-01 19:09:38 +0530

    added inview for not loading assets at once

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -2,14 +2,21 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
+import { useInView } from "react-intersection-observer";
 
 const SectionWrapper = (Component, idName) =>
   function HOC() {
+    const [ref, inView] = useInView({
+      threshold: 0.2, // when 20% of section is visible
+      triggerOnce: true, // animate only first time
+    });
     return (
       <motion.section
+        ref={ref}
         variants={staggerContainer()}
         initial='hidden'
         whileInView='show'
+        animate={inView ? { opacity: 1, y: 0 } : {}}
         viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
